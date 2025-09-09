3c63d7c1b0de177187b0637fdcc9b17678b7b280
Kong Kimmeng <mengmeng08747@gmail.com>
2025-09-09 09:44:11 +0700

    09-09

---
+++ Telegram_Web/Models/OpenCaseResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Telegram_Web.Models
+{
+    public class OpenCaseResult
+    {
+        public string EmpID { get; set; }           
+        public string Name { get; set; }
+        public string TeamName { get; set; }
+        public int OpenCaseCount { get; set; }
+        public string OpenCaseIDs { get; set; }
+        public string ChatIDs { get; set; }
+        public string ChatTitles { get; set; } // semicolon-separated
+    }
+}
